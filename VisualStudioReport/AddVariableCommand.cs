36641bfff745bec3eddd2a48b1b1c49b027ef89e
denis.kovalchuk <denis.kovalchuk@visualsvn.com>
2026-09-03 18:30:59 +0300

    Add a project with reproduction

---
+++ VisualStudioReport/AddVariableCommand.cs
@@ -0,0 +1,59 @@
+﻿using EnvDTE;
+using EnvDTE80;
+using Microsoft.VisualStudio.Shell;
+using Microsoft.VisualStudio.Shell.Interop;
+using System;
+using System.ComponentModel.Design;
+using Task = System.Threading.Tasks.Task;
+
+namespace VisualStudioReport
+{
+    internal sealed class AddVariableCommand
+    {
+        public const int CommandId = 0x0100;
+        public static readonly Guid CommandSet = new Guid("51e099f6-f729-4a4e-8b15-e6108eebecfa");
+        private readonly AsyncPackage package;
+        private readonly DTE2 dte;
+
+        private AddVariableCommand(AsyncPackage package, OleMenuCommandService commandService, DTE2 dte)
+        {
+            this.package = package ?? throw new ArgumentNullException(nameof(package));
+            commandService = commandService ?? throw new ArgumentNullException(nameof(commandService));
+            this.dte = dte;
+
+            var menuCommandID = new CommandID(CommandSet, CommandId);
+            var menuItem = new MenuCommand(this.Execute, menuCommandID);
+            commandService.AddCommand(menuItem);
+        }
+
+        public static AddVariableCommand Instance
+        {
+            get;
+            private set;
+        }
+
+        public static async Task InitializeAsync(AsyncPackage package)
+        {
+            await ThreadHelper.JoinableTaskFactory.SwitchToMainThreadAsync(package.DisposalToken);
+
+            OleMenuCommandService commandService = await package.GetServiceAsync(typeof(IMenuCommandService)) as OleMenuCommandService;
+            DTE2 dte = await package.GetServiceAsync(typeof(DTE)) as DTE2;
+            Instance = new AddVariableCommand(package, commandService, dte);
+        }
+
+        private void Execute(object sender, EventArgs e)
+        {
+            ThreadHelper.ThrowIfNotOnUIThread();
+
+            dte.Solution.Globals["Var"] = "123";
+            dte.Solution.Globals.set_VariablePersists("Var", true);
+
+            VsShellUtilities.ShowMessageBox(this.package,
+                                            "Added 'Var' with value '123'.",
+                                            string.Empty,
+                                            OLEMSGICON.OLEMSGICON_INFO,
+                                            OLEMSGBUTTON.OLEMSGBUTTON_OK,
+                                            OLEMSGDEFBUTTON.OLEMSGDEFBUTTON_FIRST);
+        }
+    }
+}
---
+++ VisualStudioReport/VisualStudioReportPackage.cs
@@ -0,0 +1,23 @@
+﻿using Microsoft.VisualStudio.Shell;
+using System;
+using System.Runtime.InteropServices;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace VisualStudioReport
+{
+    [PackageRegistration(UseManagedResourcesOnly = true, AllowsBackgroundLoading = true)]
+    [Guid(VisualStudioReportPackage.PackageGuidString)]
+    [ProvideMenuResource("Menus.ctmenu", 1)]
+    public sealed class VisualStudioReportPackage : AsyncPackage
+    {
+        public const string PackageGuidString = "11932e6d-4f36-43f9-804f-16266c27e58b";
+
+        protected override async Task InitializeAsync(CancellationToken cancellationToken, IProgress<ServiceProgressData> progress)
+        {
+            await this.JoinableTaskFactory.SwitchToMainThreadAsync(cancellationToken);
+            await AddVariableCommand.InitializeAsync(this);
+            await GetVariableCommand.InitializeAsync(this);
+        }
+    }
+}
